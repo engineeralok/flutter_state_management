055eb0f636a461969c7a60b511c0f479ccbae93b
engineeralok <engineeralok07@gmail.com>
2023-06-06 19:24:13 +0530

    mvvm architecture using provider

---
+++ mvvm_with_provider/lib/data/app_exceptions.dart
@@ -0,0 +1,27 @@
+class AppException implements Exception {
+  final _message;
+  final _prefix;
+  AppException([this._message, this._prefix]);
+
+  String toString() {
+    return '$_prefix$_message';
+  }
+}
+
+class FetchDataException extends AppException {
+  FetchDataException([String? message])
+      : super(message, 'Error During Communication');
+}
+
+class BadRequestException extends AppException {
+  BadRequestException([String? message]) : super(message, 'Invalid Request');
+}
+
+class UnAuthorizedException extends AppException {
+  UnAuthorizedException([String? message])
+      : super(message, 'Unauthorized Request');
+}
+
+class InvalidInputException extends AppException {
+  InvalidInputException([String? message]) : super(message, 'Invalid Input');
+}
